98743adb557dde0150f50d5b7391443639c8b35a
Alec <alec.sobkoviak@colorado.edu>
2017-11-09 14:07:13 -0700

    Added Firebase Connection... also added a file called .DS_Store?

--- foodies-project/App.js
+++ foodies-project/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
 import Login from './src/components/Login/Login';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA45vMMLm17hnDg33BCOFuLX-w96MZnU3Y",
+  authDomain: "group-3308-project.firebaseapp.com",
+  databaseURL: "https://group-3308-project.firebaseio.com/",
+  storageBucket: "gs://group-3308-project.appspot.com"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 
 export default class App extends React.Component {
   render() {
